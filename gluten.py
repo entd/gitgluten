1309af300763169ff3209856fb0a36c8908ae9ba
entdx <ignas.brasiskis@gmail.com>
2015-05-10 20:00:56 +0300

    Daemonized service

--- gluten.py
+++ gluten.py
@@ -2,11 +2,19 @@ import os
 import sys
 import confparser
 import server
+import daemon
+import daemon.pidfile as pidlockfile
 
 DEFAULT_NAME="config.json"
 
+pidfile = pidlockfile.TimeoutPIDLockFile("/var/run/gluten.pid")
+with daemon.DaemonContext(pidfile=pidfile):
+	main()
+
+def main():
+	configParser = confparser.ConfigParser(DEFAULT_NAME)
+	config = configParser.main_config
+	server.serve(config)
 
 if __name__ == "__main__":
-    configParser = confparser.ConfigParser(DEFAULT_NAME)
-    config = configParser.main_config
-    server.serve(config)
+	main()
